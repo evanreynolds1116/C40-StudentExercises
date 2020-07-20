567d9bd8c98b6c108b6d6e8d1fc4a0da237a0e75
evan.m.reynolds95@gmail.com <evanr95@MSI.localdomain>
2020-07-20 14:45:00 -0500

    CH 19 - Part Two

--- instructor.py
+++ instructor.py
@@ -1,8 +1,8 @@
+from person import Person
+
 class Instructor:
     def __init__(self, first_name, last_name, slack_handle, cohort, specialty):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.slack_handle = slack_handle
+        super().__init__(self, first_name, last_name, slack_handle, cohort)
         self.specialty = specialty
 
     def assign_exercise(self, exercise, student):
